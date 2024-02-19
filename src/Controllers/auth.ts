b7e72a12f5c9cd98f03f3aacec7046cdd819955f
Daniel Ofo-muogbo <Danielofomuogbo@yahoo.com>
2024-02-19 13:30:37 +0100

    new update

--- src/Controllers/auth.ts
+++ src/Controllers/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
 import { hashSync, compareSync } from "bcrypt";
 import * as jwt from "jsonwebtoken";
@@ -6,19 +6,26 @@ import { JWT_SECRET } from "../secrets";
 import { BadRequestsException } from "../Exceptions/bad-requests";
 import { ErrorCodes } from "../Exceptions/root";
 
-export const signup = async (req: Request, res: Response) => {
-  const { email, password, name } = req.body;
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+try {
+    const { email, password, name } = req.body;
+    
+    let user = await prismaClient.user.findFirst({ where: { email } });
+    if (user) {
+      next (new BadRequestsException("User already exists", ErrorCodes.USER_ALREADY_EXISTS));
+    }
+    
+    user = await prismaClient.user.create({
+      data: { email, name, password: hashSync(password, 10) },
+    });
+    
+    res.json(user);
 
-  let user = await prismaClient.user.findFirst({ where: { email } });
-  if (user) {
-    throw new BadRequestsException("User already exists", ErrorCodes.USER_ALREADY_EXISTS);
-  }
+}catch (err) {
 
-  user = await prismaClient.user.create({
-    data: { email, name, password: hashSync(password, 10) },
-  });
+    
+}
 
-  res.json(user);
 };
 
 export const login = async (req: Request, res: Response) => {
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { PORT } from "./secrets";
 import rootRouter from "./Routes";
 import { PrismaClient } from "@prisma/client";
 import { errorMiddleware } from "./Middlewares/errors";
+import { SignUpSchema } from "./schema/users";
 
 const app: Express = express();
 
@@ -13,7 +14,16 @@ app.use(errorMiddleware)
 
 export const prismaClient = new PrismaClient({
   log: ["query", "info", "warn", "error"],
-});
+}).$extends({
+    query: {
+        user: {
+            create({args, query}){
+                args.data = SignUpSchema.parse(args.data)
+                return query(args)
+            }
+        }
+    }
+})
 app.listen(PORT, () => {
   console.log("Example app listening on port 4000!");
 });
